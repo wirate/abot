d79e492b94532b4a5230b5cfe166b67bc888e4d3
Henrik Hautakoski <henrik@fiktivkod.org>
2013-09-10 16:59:27 +0200

    events/Diskspace.php: Use Relay_Measure.

--- events/Diskspace.php
+++ events/Diskspace.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Relay/Measure/Binary.php';
+
 require_once 'Relay/Event/Command.php';
 
 class DiskspaceCommand extends Relay_Event_Command
@@ -14,37 +16,16 @@ class DiskspaceCommand extends Relay_Event_Command
         $total = disk_total_space($path);
         $free = disk_free_space($path);
 
+        if ($total == 0) {
+            $left = 0;
+        } else {
+            $left = floor($free / $total * 100);
+        }
+        
+        $total = new Relay_Measure_Binary($total, Relay_Measure_Abstract::OPTIMAL, 2);
+        $free = new Relay_Measure_Binary($free, Relay_Measure_Abstract::OPTIMAL, 2);
 
-        $msg->setTrail("$path: {$this->_format($free)} / {$this->_format($total)} (" . floor($free / $total * 100) . "% left)");
+        $msg->setTrail("$path: {$free} / {$total} ({$left}% left)");
         $this->caller->write($msg->getMessage());
     }
-
-    protected function _format($size)
-    {
-        $suffix = array(
-            'T' => 1099511627776,
-            'G' => 1073741824,
-            'M' => 1048576,
-            'K' => 1024,
-        );
-
-        foreach($suffix as $u => $v) {
-            if ($size < $v) {
-                continue;
-            }
-
-            $size /= $v;
-
-            if ($size < 10) {
-                $dec = 1;
-            } else {
-                $dec = 0;
-            }
-
-            return round($size + ((1 / pow(10, 1 + $dec)) * 5), $dec) . $u;
-        }
-
-        return $size;
-    }
-
 }
