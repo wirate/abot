b4de0dc4c89d90cfdcb787d26428b45d49ecaacf
Henrik Hautakoski <henrik@fiktivkod.org>
2014-01-02 19:42:19 +0100

    library/Relay/Event/Request.php: Some minor style changes

--- library/Relay/Event/Request.php
+++ library/Relay/Event/Request.php
@@ -4,26 +4,26 @@
  */
 class Relay_Event_Request
 {
-	protected $parameters = array();
+    protected $parameters = array();
 
-	public function __construct($string, $delimiter = ' ')
-	{
-		$this->parameters = explode($delimiter, $string);
-	}
-	
-	public function getAll()
-	{
-		return $this->parameters;
-	}
-	
-	public function get($key)
-	{
-		return (isset($this->parameters[$key])) 
-			 ? $this->parameters[$key] : false;
-	}
-	
-	public function count()
-	{
-		return count($this->parameters);
-	}
+    public function __construct($string, $delimiter = ' ')
+    {
+        $this->parameters = explode($delimiter, $string);
+    }
+
+    public function getAll()
+    {
+        return $this->parameters;
+    }
+
+    public function get($key)
+    {
+        return (isset($this->parameters[$key]))
+            ? $this->parameters[$key] : false;
+    }
+
+    public function count()
+    {
+        return count($this->parameters);
+    }
 }
